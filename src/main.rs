6a1567dba1ec982eb682daeb43c8c827a459f9d5
fard-draf <fard@ik.me>
2025-05-05 12:50:35 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-// use signum::{
-//     cli::build::{self, run_interactive},
-//     core::{
-//         keys_ed25519::generate_keypair,
-//         keysfiles::{load_signing_key_from_file, save_signing_key_to_file},
-//         signer::sign_data,
-//         verifier::verify_signature,
-//         *,
-//     },
-//     error::AppError,
-// };
-// use std::io;
-
-// fn main() -> Result<(), AppError> {
-//     run_interactive();
-//     Ok(())
-// }
+fn main() {}
